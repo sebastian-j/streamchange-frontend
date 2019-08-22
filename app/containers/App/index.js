fa60cd002639dea846296d792d7b230d041cbf34
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2019-08-22 08:07:38 +0200

    change url path

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/giveaway" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
