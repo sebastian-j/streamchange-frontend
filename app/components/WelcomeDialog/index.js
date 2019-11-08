6a529dcd0df633fe64502bf1d6ac28eb3d21cf0a
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2019-11-08 19:45:55 +0100

    block app on some browsers due to incompatibility issues
    Dexie causes an error and does not create database on browsers other than Chrome. This commit will be reverted if I will find solution or Dexie will be updated.

--- app/components/WelcomeDialog/index.js
+++ app/components/WelcomeDialog/index.js
@@ -7,11 +7,12 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
+import './style.css';
 
 export default class WelcomeDialog extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isOpen: true, videoLink: '' };
+    this.state = { isOpen: true, videoLink: '', isChrome: true };
     this.sendVideoLink = this.sendVideoLink.bind(this);
     this.handleInputValueChange = this.handleInputValueChange.bind(this);
   }
@@ -20,6 +21,12 @@ export default class WelcomeDialog extends React.Component {
     if (typeof this.props.passVideo === 'function') {
       this.props.passVideo(this.state.videoLink);
     }
+  }
+
+  componentDidMount() {
+    this.setState({
+      isChrome: !!window.chrome,
+    });
   }
 
   handleInputValueChange(event) {
@@ -35,38 +42,49 @@ export default class WelcomeDialog extends React.Component {
   }
 
   render() {
+    if (this.state.isChrome) {
+      return (
+        <div className="blur">
+          <Dialog
+            open={this.state.isOpen}
+            onClose={this.closeDialog}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">
+              Wybierz, na którym streamie organizujesz giveaway
+            </DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                name="videoLink"
+                onChange={this.handleInputValueChange}
+                id="videoLink"
+                label="Wklej link do streama"
+                type="text"
+                value={this.state.videoLink}
+                fullWidth
+              />
+              <DialogContentText>
+                <span style={{ fontSize: '0.8rem' }}>
+                  Przykład: https://www.youtube.com/watch?v=CBUBY45me_A
+                </span>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.sendVideoLink} color="primary">
+                Dalej
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      );
+    }
     return (
-      <div className="blur">
-        <Dialog
-          open={this.state.isOpen}
-          onClose={this.closeDialog}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">Wybierz, na którym streamie organizujesz giveaway</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              name="videoLink"
-              onChange={this.handleInputValueChange}
-              id="videoLink"
-              label="Wklej link do streama"
-              type="text"
-              value={this.state.videoLink}
-              fullWidth
-            />
-            <DialogContentText>
-              <span style={{ fontSize: '0.8rem' }}>
-                Przykład: https://www.youtube.com/watch?v=CBUBY45me_A
-              </span>
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.sendVideoLink} color="primary">
-              Dalej
-            </Button>
-          </DialogActions>
-        </Dialog>
+      <div className="vertical">
+        <div className="compatibility-info">
+          Aplikacja działa wyłącznie w Google Chrome
+        </div>
       </div>
     );
   }
