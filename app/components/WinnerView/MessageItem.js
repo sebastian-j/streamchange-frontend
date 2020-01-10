4da1c814dcdc7594163f33177bb14bffde70428a
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2020-01-10 17:00:12 +0100

    bugfix: display time with leading zeros

--- app/components/WinnerView/MessageItem.js
+++ app/components/WinnerView/MessageItem.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 function MessageItem(props) {
   const dt = new Date(props.date);
-  const convertedDate = `${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`;
+  const convertedDate = `${dt.getHours()}:${
+    dt.getMinutes() < 10 ? '0' : ''
+  }${dt.getMinutes()}:${dt.getSeconds() < 10 ? '0' : ''}${dt.getSeconds()}`;
   return (
     <li className="winner-message">
       <span>{convertedDate}</span>
