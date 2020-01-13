0fa658c87a5745ef750a88ed3a3d3859656d8af5
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2020-01-13 21:08:07 +0100

    rebuild WelcomeDialog
    New WelcomeDialog shows random hint under stream url input.
    WelcomeDialog is now made without material-ui for better performance.

--- app/components/WelcomeDialog/index.js
+++ app/components/WelcomeDialog/index.js
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
+import { HINTS } from '../../config';
 import './style.css';
 
 const WelcomeDialog = props => {
-  const [isOpen, setIsOpen] = useState(true);
   const [videoLink, setVideoLink] = useState('');
-  const [isChrome, setIsChrome] = useState('');
+  const [isChrome, setIsChrome] = useState(true);
+  const [hint, setHint] = useState('');
 
   const sendVideoLink = () => {
     if (typeof props.passVideo === 'function') {
@@ -22,6 +18,7 @@ const WelcomeDialog = props => {
 
   useEffect(() => {
     setIsChrome(!!window.chrome);
+    setHint(HINTS[Math.floor(Math.random() * HINTS.length)]);
   }, []);
 
   const handleInputValueChange = event => {
@@ -39,40 +36,53 @@ const WelcomeDialog = props => {
   if (isChrome) {
     return (
       <div className="blur">
-        <Dialog open={isOpen} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">
-            Wybierz, na którym streamie organizujesz giveaway
-          </DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              name="videoLink"
-              onChange={handleInputValueChange}
-              onKeyPress={handleKeyPress}
-              id="videoLink"
-              label="Wklej link do streama"
-              type="text"
-              value={videoLink}
-              fullWidth
-            />
-            <DialogContentText>
-              <span style={{ fontSize: '0.8rem' }}>
-                Przykład: https://www.youtube.com/watch?v=CBUBY45me_A
-              </span>
-              {props.error && (
-                <span style={{ display: 'block', color: '#bd0013' }}>
-                  {props.error}
+        <div className="welcome-wrapper">
+          <div className="welcomeDialog">
+            <div className="welcomeDialog-title">
+              Wybierz, na którym streamie organizujesz giveaway
+            </div>
+            <div className="welcomeDialog-content">
+              <TextField
+                autoFocus
+                margin="dense"
+                name="videoLink"
+                onChange={handleInputValueChange}
+                onKeyPress={handleKeyPress}
+                id="videoLink"
+                label="Wklej link do streama"
+                type="text"
+                value={videoLink}
+                fullWidth
+              />
+              <div className="welcomeDialog-textSecondary">
+                <span style={{ fontSize: '0.8rem' }}>
+                  Przykład: https://www.youtube.com/watch?v=CBUBY45me_A
                 </span>
-              )}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={sendVideoLink} color="primary">
-              Dalej
-            </Button>
-          </DialogActions>
-        </Dialog>
+                {props.error && (
+                  <span style={{ display: 'block', color: '#bd0013' }}>
+                    {props.error}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div className="welcomeDialog-actions">
+              <Button onClick={sendVideoLink} color="primary">
+                Dalej
+              </Button>
+            </div>
+          </div>
+          <div className="welcomeHint">
+            <img
+              src="/static/light-bulb.png"
+              height="140px"
+              alt="light bulb"
+            />
+            <div>
+              <span className="welcomeHint-title">Czy wiesz, że...</span>
+              <span className="welcomeHint-content">{hint}</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- app/config.js
+++ app/config.js
@@ -2,3 +2,5 @@
 export const API_KEY = 'YOUR_API_KEY';
 // Type your api endpoint for telemetry
 export const TELEMETRY_URL = 'URL';
+// Type hints that will display on welcome screen
+export const HINTS = ['hint1', 'hint2'];
