85d40ae363ee115bf6a46dbce192ff1138ee71df
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2020-01-16 15:52:21 +0100

    add ThemeProvider and two themes

---
+++ app/theme.js
@@ -0,0 +1,23 @@
+export const lightTheme = {
+  body: '#E2E2E2',
+  panelBackground: '#FFF',
+  buttonBackground: '#FFF',
+  buttonBackgroundHover: '#0059a3',
+  buttonTextColor: '#0059a3',
+  buttonTextColorHover: '#FFF',
+  staticTextColor: '#000',
+  activeUser: '#0a85ff',
+  inactiveUser: '#9c9c9c',
+};
+
+export const darkTheme = {
+  body: '#363537',
+  panelBackground: '#474747',
+  buttonBackground: '#474747',
+  buttonBackgroundHover: '#585858',
+  buttonTextColor: '#FAFAFA',
+  buttonTextColorHover: '#FABAFC',
+  staticTextColor: '#FFF',
+  activeUser: '#0a85ff',
+  inactiveUser: '#9c9c9c',
+};
