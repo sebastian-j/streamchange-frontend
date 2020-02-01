c93357d7687781645dbe095f7a02e7e3ec9b64f9
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2020-02-01 08:59:21 +0100

    add Enzyme tests

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,3 +1,6 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import '@babel/polyfill';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
