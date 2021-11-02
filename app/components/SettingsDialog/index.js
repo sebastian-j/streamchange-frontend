22f2bc6a8d0eeba500764e88dd16040234082edd
Sebastian Jankowski <38406394+sebastian-j@users.noreply.github.com>
2021-11-02 18:15:20 +0100

    improve top menu layout
    Delete SettingsButton styled component and replace it with unified ToolbarButton.

--- app/components/SettingsDialog/index.js
+++ app/components/SettingsDialog/index.js
@@ -18,23 +18,9 @@ import messages from './messages';
 import ColorPicker from '../ColorPicker';
 import DarkModeSwitch from './DarkModeSwitch';
 import LocaleToggle from './LocaleToggle';
+import ToolbarButton from '../SupportInformation/ToolbarButton';
 import { makeSelectColor } from '../../containers/StyleProvider/selectors';
 import { changeColor } from '../../containers/StyleProvider/actions';
-
-const SettingsButton = styled.button`
-  background: ${(props) => props.theme.buttonBackground};
-  border: 1px solid ${(props) => props.theme.color};
-  color: ${(props) => props.theme.color};
-  border-radius: 4px;
-  height: 80%;
-  padding: 3px 5px;
-  margin: 10px 15px 0 0;
-  text-decoration: none;
-  &:hover {
-    background-color: ${(props) => props.theme.buttonBackgroundHover};
-    color: ${(props) => props.theme.buttonTextColorHover};
-  }
-`;
 
 const HintParagraph = styled.span`
   font-size: 0.9rem;
@@ -84,16 +70,24 @@ const SettingsDialog = (props) => {
 
   return (
     <div style={{ display: 'inline-block' }}>
-      <SettingsButton onClick={openDialog} type="button">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" />
-        </svg>
-      </SettingsButton>
+      <Tooltip
+        title={
+          <HintParagraph>
+            <FormattedMessage {...messages.dialogTitle} />
+          </HintParagraph>
+        }
+      >
+        <ToolbarButton onClick={openDialog} type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" />
+          </svg>
+        </ToolbarButton>
+      </Tooltip>
       <Dialog
         open={isOpen}
         onClose={closeDialog}
