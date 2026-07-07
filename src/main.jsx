afae145049254a4f14b696cf30fcb0e074fab7d7
Kacper <kkubacki0404@gmail.com>
2026-07-07 14:58:10 +0200

    cherry-pick z dodania języków

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { translationMessages, DEFAULT_LOCALE } from './i18n';
-
+import LanguageProvider from './containers/LanguageProvider';
+import StyleProvider from './containers/StyleProvider'; 
 import configureStore from './configureStore';
 import App from './App';
+import 'sanitize.css/sanitize.css';
 
 const store = configureStore({});
 const container = document.getElementById('app');
