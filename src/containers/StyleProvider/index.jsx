653d84fa2bb0231b77991e475a2866088cfbda98
Kacper <kkubacki0404@gmail.com>
2026-07-07 13:05:52 +0200

    cherrypick z styleprovidera

--- src/containers/StyleProvider/index.jsx
+++ src/containers/StyleProvider/index.jsx
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
-
 import { makeSelectColor, makeSelectDarkMode } from './selectors';
 import { darkTheme, lightTheme } from '../../theme';
-
 export function StyleProvider(props) {
   const themeType = props.isDarkMode ? darkTheme : lightTheme;
   const theme = { ...themeType, color: props.color };
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,24 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-
 import { IntlProvider } from 'react-intl';
 import { translationMessages, DEFAULT_LOCALE } from './i18n';
-
+import LanguageProvider from './containers/LanguageProvider';
+import StyleProvider from './containers/StyleProvider'; 
 import configureStore from './configureStore';
 import App from './App';
+import 'sanitize.css/sanitize.css';
 
 const initialState = {};
 const store = configureStore(initialState);
-
-const theme = {
-  bodyBackground: '#121212',
-};
-
 const container = document.getElementById('app');
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <IntlProvider
-      locale={DEFAULT_LOCALE}
-      messages={translationMessages[DEFAULT_LOCALE]}
-    >
-      <ThemeProvider theme={theme}>
+    <IntlProvider locale={DEFAULT_LOCALE} messages={translationMessages[DEFAULT_LOCALE]}>
+      <StyleProvider>
         <App />
-      </ThemeProvider>
+      </StyleProvider>
     </IntlProvider>
   </Provider>
 );
