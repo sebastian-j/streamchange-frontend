72c5101f4f653e1df554eab677e12e8a16d274e6
Kacper <kkubacki0404@gmail.com>
2026-07-07 14:20:46 +0200

    cherry-pick z dodania języków

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,20 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-
-import { IntlProvider } from 'react-intl';
 import { translationMessages, DEFAULT_LOCALE } from './i18n';
 
 import configureStore from './configureStore';
 import App from './App';
 
-const initialState = {};
-const store = configureStore(initialState);
-
-const theme = {
-  bodyBackground: '#121212',
-};
-
+const store = configureStore({});
 const container = document.getElementById('app');
 const root = createRoot(container);
-
 root.render(
   <Provider store={store}>
-    <IntlProvider
-      locale={DEFAULT_LOCALE}
-      messages={translationMessages[DEFAULT_LOCALE]}
-    >
-      <ThemeProvider theme={theme}>
+    <LanguageProvider messages={translationMessages}>
+      <StyleProvider>
         <App />
-      </ThemeProvider>
-    </IntlProvider>
+      </StyleProvider>
+    </LanguageProvider>
   </Provider>
 );
