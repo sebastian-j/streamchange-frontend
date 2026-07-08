cf5def6f4073e1539681add9348580309b396c2b
KrzysztofPieczka <kpieczka@student.agh.edu.pl>
2026-07-08 15:27:10 +0200

    fix

--- src/components/YoutubeWorker/db.jsx
+++ src/components/YoutubeWorker/db.jsx
@@ -2,12 +2,9 @@ import Dexie from 'dexie';
 
 const db = new Dexie('Giveaway');
 db.version(1).stores({
-  users: 'id, imageUrl, title, message, isModerator, isSponsor, isEligible',
+  users: 'id, imageUrl, title, message, isModerator, isSubscriber, isEligible',
   queue: 'id, imageUrl, title, message, addedAt, lastActiveAt',
   messages: '++id, authorId',
   history: '++id, channelId',
-});
-db.version(2).stores({
-  users: 'id, imageUrl, title, message, isModerator, isSubscriber, isEligible',
 });
 export default db;
