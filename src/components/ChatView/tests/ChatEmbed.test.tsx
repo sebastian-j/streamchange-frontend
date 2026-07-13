a814feaaad7accf34a03afbb829e9f0d3e21041f
Sebastian Jankowski <sebastian-j@users.noreply.github.com>
2026-07-13 17:15:14 +0200

    add redux store to the ChatEmbed

--- src/components/ChatView/tests/ChatEmbed.test.tsx
+++ src/components/ChatView/tests/ChatEmbed.test.tsx
@@ -1,16 +1,34 @@
 import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { Provider } from 'react-redux';
+import configureStore, { MockStoreEnhanced } from 'redux-mock-store';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 import ChatEmbed from '../ChatEmbed';
 
+const mockStore = configureStore([]);
+
 describe('<ChatEmbed />', () => {
+  let store: MockStoreEnhanced<unknown, {}>;
+  beforeEach(() => {
+    store = mockStore({
+      isDarkMode: false,
+    });
+  });
   it('should render Twitch chat and match the snapshot', () => {
-    const { container } = render(<ChatEmbed channel="id" platform="twitch" />);
+    const { container } = render(
+      <Provider store={store}>
+        <ChatEmbed channel="id" platform="twitch" />
+      </Provider>
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should render Kick chat and match the snapshot', () => {
-    const { container } = render(<ChatEmbed channel="id" platform="kick" />);
+    const { container } = render(
+      <Provider store={store}>
+        <ChatEmbed channel="id" platform="kick" />
+      </Provider>
+    );
     expect(container.firstChild).toMatchSnapshot();
   });
 });
