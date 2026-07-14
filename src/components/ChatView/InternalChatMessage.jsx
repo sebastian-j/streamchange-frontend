1c71ea74e2ae0e5ffbd3fa017d45ac977653e202
KrzysztofPieczka <kpieczka@student.agh.edu.pl>
2026-07-14 13:09:37 +0200

    overflow-wrap internalchat fix

--- src/components/ChatView/InternalChatMessage.jsx
+++ src/components/ChatView/InternalChatMessage.jsx
@@ -26,6 +26,7 @@ const AuthorTitle = styled.span`
 `;
 const MessageText = styled.span`
   color: ${(props) => props.theme.staticTextColor};
+  overflow-wrap: anywhere;
 `;
 
 const EmoteImg = styled.img`
