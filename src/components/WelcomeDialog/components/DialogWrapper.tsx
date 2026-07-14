8776d93ef17272d7956603bef0b8f5e587ed08fe
KrzysztofPieczka <kpieczka@student.agh.edu.pl>
2026-07-14 10:05:54 +0200

    oneline deleted

--- src/components/WelcomeDialog/components/DialogWrapper.tsx
+++ src/components/WelcomeDialog/components/DialogWrapper.tsx
@@ -38,7 +38,6 @@ const DialogWrapper = styled.div`
       0 24px 38px 3px rgba(0, 0, 0, 0.14),
       0 9px 46px 8px rgba(0, 0, 0, 0.52);
     border-radius: 4px;
-    width: 100%;
     z-index: -1;
 
     .title {
