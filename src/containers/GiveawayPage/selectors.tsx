7b5e2fb7ae5e61f01bd00a163f6d818c4205e69f
Krzysztof Pieczka <kpieczka@student.agh.edu.pl>
2026-07-21 14:33:35 +0200

    all errors fixed + static type check turned on (#122)
    * all errors fixed + static type check turned on
    
    * test

--- src/containers/GiveawayPage/selectors.tsx
+++ src/containers/GiveawayPage/selectors.tsx
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { ApplicationRootState } from 'types';
+import { ApplicationRootState } from '../../components/types';
 import { initialState } from './reducer';
 
 /**
