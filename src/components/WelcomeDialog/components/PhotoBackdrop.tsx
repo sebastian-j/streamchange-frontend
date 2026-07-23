6829c9f09f71d15ed127e27dc00209f6354a7e17
KrzysztofPieczka <kpieczka@student.agh.edu.pl>
2026-07-23 14:59:49 +0200

    review changes

--- src/components/WelcomeDialog/components/PhotoBackdrop.tsx
+++ src/components/WelcomeDialog/components/PhotoBackdrop.tsx
@@ -2,14 +2,9 @@ import styled from 'styled-components';
 import coverImage from '../assets/background.png';
 
 export const PhotoBackdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-
-  background-image: url(${coverImage});
-  background-repeat: no-repeat;
-  background-position: center center;
+  background: url(${coverImage}) no-repeat center center fixed;
   background-size: cover;
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
 `;
