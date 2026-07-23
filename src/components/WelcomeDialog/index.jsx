ece0ba74b9ea8006dcc4a345d14452676d061ce6
KrzysztofPieczka <kpieczka@student.agh.edu.pl>
2026-07-23 15:41:26 +0200

    fix

--- src/components/WelcomeDialog/index.jsx
+++ src/components/WelcomeDialog/index.jsx
@@ -10,7 +10,7 @@ import DialogWrapper from './components/DialogWrapper';
 import FirstUseScreen from './FirstUseScreen';
 import { PhotoBackdrop } from './components/PhotoBackdrop';
 import WavyButton from './components/WavyButton';
-import WelcomeHint from './WelcomeHint';
+//import WelcomeHint from './WelcomeHint';
 
 const CHANNEL_URL_REGEX =
   /^(?:https?:\/\/)?(?:www\.)?(twitch\.tv|kick\.com)\/([a-zA-Z0-9_-]+)(?:[/?#].*)?$/i;
