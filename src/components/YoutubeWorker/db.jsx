d12f9bf964dd56403fc03168883531ad51784e04
Kacper <kkubacki0404@gmail.com>
2026-07-31 18:35:04 +0200

    dodany youtube worker i embed

--- src/components/YoutubeWorker/db.jsx
+++ src/components/YoutubeWorker/db.jsx
@@ -2,9 +2,10 @@ import Dexie from 'dexie';
 
 const db = new Dexie('Giveaway');
 db.version(1).stores({
-  users: 'id, imageUrl, title, message, isModerator, isSponsor, isEligible',
+  users: 'id, imageUrl, title, message, isModerator, isSubscriber, isEligible',
   queue: 'id, imageUrl, title, message, addedAt, lastActiveAt',
   messages: '++id, authorId',
   history: '++id, channelId',
 });
 export default db;
+
